ab697985eebbfafaabaa93332f6829fd4d87752b
Carlos Eduardo González Cortes <carlosedugoc@gmail.com>
2019-12-23 23:01:57 +1300

    Updates

---
+++ precache-manifest.223a124e04f6ccd8250108c48a085f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef21277b07a859ded581ce4c7336bb80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a699f34257cdfe7a896",
+    "url": "/monsters-rolodex/static/css/main.1b54e19a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a699f34257cdfe7a896",
+    "url": "/monsters-rolodex/static/js/main.1baced2f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
